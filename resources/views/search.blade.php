bc6465cbf762f18e278fe709583aea011d0f3650
cley9 <cleyutp@gmail.com>
2022-09-28 01:56:56 -0500

    versión 0.16 design list search producto

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -32,23 +32,17 @@
     </div>
         @endforeach
     </div>
+
  <div class="row row-cols-2 row-cols-md-6">
 @foreach ($search as $itemSearch)
      {{--  <h6>{{$itemSearch->nombre}}</h6>  --}}
+     <div class="col mb-5 ">
+       <div class="card  box-efect box-love" >
+       {{--  <div class="overlay icons--love--font">
+         <a  onclick="return love();" class="icon icons--font-love "  ><i class="bi bi-heart"></i></a>
+        </div>  --}}
 
-     <div class="col mb-5">
-
-
-        <div class="card  box-efect box-love" >
-        <div class="overlay icons--love--font">
-
-       <a  onclick="return love();" class="icon icons--font-love "  ><i class="bi bi-heart"></i></a>
-
-        </div>
-
-
-
-        <div class="card shadow ">
+        <div class="card shadow box--search--yes">
       <div class="">
             <a href="{{route('View.home.index', $id=$itemSearch->id)}}">
             <img src="{{asset('storage/img/Productos/'.$itemSearch->imagen.'')}}" class="card-img-top" width="200px"/>
@@ -56,22 +50,30 @@
         </div>
           <div class="card-body">
             <div class="d-flex  align-items-center ">
-                                          <h5 class="card-title h6">{{$itemSearch->nombre}}</h5>
+                                          <h5 class="card-title h6 title--box--pro">{{$itemSearch->nombre}}</h5>
                                       </div>
-                                   <div class="mb-0 d-flex justify-content-between  ">
-                                   <span><del>s/.{{$itemSearch->precio}}</del></span>
-                                   <span>s/.{{$itemSearch->newPrecio}}</span>
+                                   <div class="mb-1 mb-md-0 d-flex justify-content-between  ">
+                                   <span class="box--text--pre"><del>S/ {{number_format($itemSearch->precio, 2, ".", ",")}}</del></span>
+                                   <span class="box--text--pre">S/ {{number_format($itemSearch->newPrecio, 2, ".", ",")}}</span>
                                         </div>
             <div class=" align-items-center d-flex ">
      <span class="h6 mb-0 text-muted fw-normal">
 
-     <small>{{$itemSearch->cantidad}} disponibles </small>  </span>
+     <small class="box--text--pre">{{$itemSearch->cantidad}} disponibles </small>  </span>
      </div>
        <hr>
             <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
+                <div class="col-12 btn-group d-flex justify-content-center">
                     {{--  <a href="view-page.php?id_view=<?php echo $item['db_id'] ?>" class="btn btn-sm btn-outline-secondary">Ver</a>  --}}
-                    <a onclick="return love();"  class="btn btn-sm btn-outline-primary " >Agregar</a>
+        @if (session()->exists('name') && session()->get('rol') === '0')
+        <a class="btn--addCard--countMaster btn-sm text-dark viewCP"
+        id="addProCard{{ $itemSearch->id}}" onclick=" addProCart({{ $itemSearch->id }}, 1, {{ $itemSearch->newPrecio }},
+        '{{ $itemSearch->nombre }}', '{{ $itemSearch->imagen }}')">Agregar</a>
+        @else
+
+        <a onclick="msjInicieSesion()" class="btn btn--view-add btn-sm " >Agregar </a>
+        @endif
+                    {{--  <a onclick="return love();"  class="btn btn-sm btn-outline-primary " >Agregar</a>  --}}
                 </div>
             </div>
           </div>
@@ -81,5 +83,7 @@
 @endforeach
 </div>
 </div>
+
+@include('User.modelAddProCart')
 
     @endsection
