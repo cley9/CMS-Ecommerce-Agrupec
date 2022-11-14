6dba0dc1969fc806d33d37ee0ff74462a0f3c40f
cley9 <cleyutp@gmail.com>
2022-11-14 02:40:32 -0500

    version 0.33

--- app/Http/Controllers/user/PdfTickedController.php
+++ app/Http/Controllers/user/PdfTickedController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\user;
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\User;
-use Barryvdh\DomPDF\PDF;
-// use Barryvdh\DomPDF\Facade as PDF;
+// use Barryvdh\DomPDF\PDF;
+use Barryvdh\DomPDF\Facade as PDF;
 
 use Illuminate\Http\Request;
 
@@ -22,11 +22,19 @@ class PdfTickedController extends Controller
         // $users_ = $users->find(2);
         // $users = Cart::with(['cart'])->find(session()->get('userId'));
         // echo $user->productos;
-        $ticket = $user->productos;
         // echo $ticket;
-        $exitProductoUser = cart::where('userId', session()->get('userId'))->where('productoId', '>', 0)->exists();
         // return view('User.Pdf.pdfDownload', compact(['cartUser', 'exitProductoUser']));
-        return view('User.Pdf.pdfDownload', compact(['ticket', 'exitProductoUser']));
+        $ticket = $user->productos;
+        // $exitProductoUser = cart::where('userId', session()->get('userId'))->where('productoId', '>', 0)->exists();
+        // return view('User.Pdf.pdfDownload', compact(['ticket', 'exitProductoUser']));
+        // Pdf::setOption(['dpi' => 150, 'defaultFont' => 'sans-serif']);
+        view()->share('User.Pdf.pdfDownload', $ticket);
+        $pdf = PDF::loadView('User.Pdf.pdfDownload', ['ticket' => $ticket]);
+        // return $pdf->download('archivo.pdf');  //----descargar
+        return $pdf->stream();  //----- this is for view online
+
+
+
     }
 
     // $cartArray = User::with(['productos'])->find(session()->get('userId')); // id user
