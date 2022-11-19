1df856553c684bbbb344daea507bdf845997ea84
cley9 <cleyutp@gmail.com>
2022-11-19 04:03:30 -0500

    version 0.47

--- resources/views/User/modalDestino.blade.php
+++ resources/views/User/modalDestino.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Seleccione el lugar</h5>
+                <h5 class="modal-title" id="staticBackdropLabel">Precio de envio</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body  row d-flex aling-items-center ">
