3e45e4c3208089a3b7aabe147ab362ede4c5a978
cley9 <cleyutp@gmail.com>
2023-08-23 02:42:21 -0500

    v0.063 modification of ruta los css a vite + ul build css

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import laravel from 'laravel-vite-plugin';
 // import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: '/public/',
     plugins: [
         laravel([
             'resources/css/app.css',
