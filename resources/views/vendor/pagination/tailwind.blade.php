26e1d5a59a42cda1ba4a489cb9f5bc3d705f7fbb
cley9 <cleyutp@gmail.com>
2023-09-08 21:40:16 -0500

    v0.064.13 modification of ruta los css a vite + ul build css + ruta relativa of icons  + botton add product + boton de pagination dising

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -1,7 +1,6 @@
 @if ($paginator->hasPages())
         <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="d-flex aling-items-center justify-content-center mb-4 ">
-        <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-            <div>
+        <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between bodyBtnPagination">
                     <span class="relative z-0 inline-flex  rounded-md  text-center">
                     {{-- Previous Page Link --}}
                     @if ($paginator->onFirstPage())
@@ -13,11 +12,12 @@
                             </span>
                         </span>
                     @else
-                        {{--  <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="{{ __('pagination.previous') }}">
+                         {{-- <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class=" relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="{{ __('pagination.previous') }}"> --}}
+                            <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="btn--pagination" aria-label="{{ __('pagination.previous') }}">
                             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
                             </svg>
-                        </a>  --}}
+                        </a> 
                     @endif
 
                     {{-- Pagination Elements --}}
@@ -37,7 +37,7 @@
                                         <span class="btn--pagination">{{ $page }}</span>
                                     </span>
                                 @else
-                                    <a href="{{ $url }}" class="btn--pagination " aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
+                                    <a href="{{ $url }}" class="btn--pagination" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
                                         {{ $page }}
                                     </a>
                                 @endif
@@ -62,7 +62,6 @@
                         </span>
                     @endif
                 </span>
-            </div>
         </div>
     </nav>
 @endif
