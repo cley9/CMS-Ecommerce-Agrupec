a77a60277c15762c7753b1f532de6b592b54309c
cley9 <cleyutp@gmail.com>
2023-10-25 13:22:44 -0500

    v0.067.1 adding page the venta in admin + [se quito los textos de home que no se usan] + [modify de correo]

--- config/constants.php
+++ config/constants.php
@@ -3,7 +3,7 @@ return [
     'numContactWhats' => '927974275',
     'numContact' => "927 974 275",
     'gmailContact' => "mensaje@agrupec.com",
-    'gmailPersonal' => "cleyutp@gmail.com",
+    'gmailPersonal' => "cleytorneromondalgo@gmail.com",
     'directionContact' => "Av.Gran Chimu Zarate, San Juan Lurigancho - Lima - Peru",
     'numProvincia' => '01 723 7349',
     'nameProyect' => 'Agrupec'
