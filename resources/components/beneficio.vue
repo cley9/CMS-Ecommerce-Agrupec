4aed2165aeb6f0c1968839aa76f5737b1f547801
cley9 <cleyutp@gmail.com>
2023-11-08 03:17:41 -0500

    v0.067.2 adding page the venta in admin + [se quito los textos de hom…] + [ se mejoro el responsi de slay de beneficio ]

--- resources/components/beneficio.vue
+++ resources/components/beneficio.vue
@@ -1,81 +1,65 @@
 <template>
-    <div class="container d-flex justify-content-center boxListPromoImg- boxPresenProHome">
-        
-        
-        <div class="row d-flex justify-content-center">
-            <div class="col-sm-12 col-md-8 col-lg-12 d-flex justify-content-center mb-3">
-                    <h2 class="textTitleVenHome text-center">¡Conoce los beneficios <small style="color:black">que ofrecemos
-                            para que tu compra sea un exito!</small></h2>
-                </div>
-            <!-- <swiper :slidesPerView="4" :spaceBetween="30" :pagination="{ -->
-                <div class="col-sm-12 col-md-8 col-lg-12">
-            <swiper :slidesPerView="isMobile ? 1 : 4" :spaceBetween="30" :pagination="{
-                clickable: true,    
-            }" :modules="modules" class="mySwiper  boxBeneficioHome">
-                <swiper-slide class="boxBeneItems">
-                    <div class="col-2 boxInformationHome ">
-                        <div class="d-flex justify-content-center boxItemImgHome">
-                            <img class="icons--items--home- iconIntemHome img-fluid -card-img-top "
-                                :src="'storage/img/icons/app-store-app.png'" alt="">
-                        </div>
-                        <div class="text-center">
-                            <h6 class="h3 mb-3">Compra</h6>
-                            <p class="h9 text-muted- font-monospace lh-base-">Ahora también compra productos desde tu
+    <div class="containerSlayHome">
+        <div class="conItemInfo">
+            <h2 class="textTitleVenHome">¡Conoce los beneficios <small style="color:black">que
+                    ofrecemos
+                    para que tu compra sea un exito!</small></h2>
+            <div class="boxSlayderMain">
+                <swiper :slidesPerView="isMobile ? 1 : 4" :spaceBetween="6" :pagination="{
+                    clickable: true,
+                }" :modules="modules">
+                    <swiper-slide>
+                        <div class="infoProBox">
+                            <div class="iconHeadSlay">
+                                <img class="top iconIntemSlay" :src="'storage/img/icons/app-store-app.png'" alt="">
+                            </div>
+                            <h6 class="compraTextBody">Compra</h6>
+                            <p class="font-monospace descripTextBody">
+                                Ahora también compra productos desde tu
                                 celular y
                                 deja que nos encarguemos del envió.</p>
                         </div>
-                    </div>
-                </swiper-slide>
-                <swiper-slide class="boxBeneItems">
-
-                    <div class="col-2 boxInformationHome">
-                        <div class="d-flex justify-content-center boxItemImgHome">
-                            <img class="icons--items--home- iconIntemHome img-fluid -card-img-top "
-                                :src="'storage/img/icons/store.png'" alt="">
-                        </div>
-                        <div class="text-center">
-                            <h6 class="h3 mb-3">Locales</h6>
-                            <p class="h9 text-muted- font-monospace lh-base-">Nos encontramos actualmente en lima en el
+                    </swiper-slide>
+                    <swiper-slide>
+                        <div class="infoProBox">
+                            <div class="iconHeadSlay">
+                                <img class="top iconIntemSlay" :src="'storage/img/icons/store.png'" alt="">
+                            </div>
+                            <h6 class="compraTextBody">Locales</h6>
+                            <p class="font-monospace descripTextBody">
+                                Nos encontramos actualmente en lima en el
                                 distrito de San Juan de Lurigancho.</p>
                         </div>
-                    </div>
-                </swiper-slide>
-                <swiper-slide class="boxBeneItems">
-
-                    <div class="col-2 boxInformationHome">
-                        <div class="d-flex justify-content-center boxItemImgHome">
-                            <img class="icons--items--home- iconIntemHome img-fluid -card-img-top "
-                                :src="'storage/img/icons/delivery-truck.png'" alt="">
-                        </div>
-                        <div class="text-center">
-                            <h6 class="h3 mb-3">Envios Perú</h6>
-                            <p class="h9 text-muted- font-monospace lh-base-">Enviamos productos a todos los departamentos
+                    </swiper-slide>
+                    <swiper-slide>
+                        <div class="infoProBox">
+                            <div class="iconHeadSlay">
+                                <img class="top iconIntemSlay" :src="'storage/img/icons/delivery-truck.png'" alt="">
+                            </div>
+                            <h6 class="compraTextBody">Envios Perú</h6>
+                            <p class="font-monospace descripTextBody">
+                                Enviamos productos a todos los departamentos
                                 del
                                 Perú de manera segura y puntual.</p>
                         </div>
-                    </div>
-                </swiper-slide>
-                <swiper-slide class="boxBeneItems">
-
-                    <div class="col-2 boxInformationHome">
-                        <div class="d-flex justify-content-center boxItemImgHome">
-                            <img class="icons--items--home- iconIntemHome img-fluid -card-img-top "
-                                :src="'storage/img/icons/customer-service.png'" alt="">
-                        </div>
-                        <div class="text-center">
-                            <h6 class="h3 mb-3">Atención 24/7</h6>
-                            <p class="h9 text-muted- font-monospace lh-base-">Estamos para ayudarte en tus consultas o
+                    </swiper-slide>
+                    <swiper-slide>
+                        <div class="infoProBox">
+                            <div class="iconHeadSlay">
+                                <img class="iconIntemSlay" :src="'storage/img/icons/customer-service.png'" alt="">
+                            </div>
+                            <h6 class="h3- mb-3- compraTextBody text-center-">Atención 24/7</h6>
+                            <p class="font-monospace descripTextBody">
+                                Estamos para ayudarte en tus consultas o
                                 brindarte información de nuevos productos.</p>
                         </div>
-                    </div>
-                </swiper-slide>
-            </swiper>
+                    </swiper-slide>
+                </swiper>
+            </div>
         </div>
-        </div>
-
-        </div>
-
+    </div>
 </template>
+
 <script>
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue';
@@ -83,7 +67,6 @@ import { Swiper, SwiperSlide } from 'swiper/vue';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-// import './style.css';
 // import required modules
 import { Pagination } from 'swiper/modules';
 
@@ -108,46 +91,155 @@ export default {
         };
     },
 };
-
-
 </script>
 
 <style scoped>
+@media(width >1500px) {
+    .swiper {
+        margin-left: 160px;
+        margin-right: 160px;
+        /* background: olivedrab !important; */
+    }
 
-@media(width >= 1200px){
+    .textTitleVenHome {
+        text-align: center;
+        margin-left: 120px;
+        margin-right: 120px;
+        margin-bottom: 30px;
+    }
 
-    .boxBeneficioHome{
-    /* width: 68rem; */
-    width: 1050px;
-}
-.boxBeneItems{
-    /* background: red !important; */
-    /* width: 40px !important; */
-    margin-right: 25px !important;
-    /* display: flex !important;
-    justify-content: center !important; */
-}
+    .conItemInfo {
+        width: 80%;
+        padding: 60px;
+    }
+
+    .swiper-slide {
+        /* background: blue !important; */
+        min-height: 418px !important;
+        max-height: 418px !important;
+    }
 }
 
-/* @media(width < 1200px){
-    .boxBeneficioHome{
-        background: blue !important;
+@media (min-width: 618px) and (max-width: 1500px) {
+
+    /* @media(418px  <= width <= 1500px) { */
+    .textTitleVenHome {
+        text-align: center;
+        margin-left: 0px;
+        margin-right: 0px;
+        margin-bottom: 30px;
+
+    }
+
+    .conItemInfo {
+        width: 94%;
+        padding: 20px;
+    }
+
+    .swiper-slide {
+        /* background: rgb(192, 168, 216) !important; */
+        display: grid;
+        grid-template-columns: auto;
+        padding: 23px;
+        border-radius: 8px;
+        place-items: center;
+        min-height: 398px !important;
+        max-height: 398px !important;
+        /* text-align: center; */
+    }
+
 }
-.boxBeneItems{
-    background: red !important;
-    width: 230px !important;
-    margin-right: 25px !important;
+
+@media(width <618px) {
+    .swiper {
+        /* background: olivedrab !important; */
+    }
+
+    .textTitleVenHome {
+        text-align: center;
+        margin-left: 0px;
+        margin-right: 0px;
+    }
+
+    .conItemInfo {
+        width: 94%;
+        padding: 20px;
+    }
+
+    .boxSlayderMain {
+        /* background: rgb(47, 153, 23) !important; */
+        padding: 30px !important;
+    }
 }
-} */
-@media(width <= 700px){
- 
-.boxBeneficioHome{
-    width: 390px;
+
+.containerSlayHome {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 120px;
 }
-.boxBeneItems{
-    /* background: orange !important; */
-    display: flex !important;
-    justify-content: center !important;
-}   
+
+.textTitleVenHome {
+    text-align: center;
 }
-</style>  
+
+.conItemInfo {
+    background: #F4F5F6;
+    border-radius: 10px;
+}
+
+.swiper-slide {
+    display: grid;
+    grid-template-columns: auto;
+    padding: 23px;
+    border-radius: 8px;
+    place-items: center;
+    background: white;
+    border: 1px solid #DFDFDF;
+    box-shadow: 1px 2px 3px 1px #E1E5E5;
+    /* box-shadow: 1px 2px 3px 1px #F4F7F7; */
+    /* box-shadow: 13px 23px 23px 1px #F4F7F7; */
+    border-radius: 8px;
+    /* text-align: center; */
+}
+
+.swiper-slide:hover {
+    box-shadow: 2px 2px 12px 2px #F4F7F7;
+    /* box-shadow: 2px 2px 12px 2px #eeeeee; */
+    transition: 0.3s ease;
+}
+
+.iconIntemSlay {
+    width: 34%;
+    height: 100%;
+    object-fit: cover;
+    /* background: skyblue; */
+}
+
+.iconHeadSlay {
+    /* background: steelblue; */
+    text-align: center;
+    margin-bottom: 48px;
+}
+
+.infoProBox {
+    margin: 32px 0;
+
+}
+
+.compraTextBody {
+    /* background: bisque; */
+    display: flex;
+    align-content: center;
+    display: grid;
+    place-content: center;
+    /* size: 100px; */
+    font-family: 'Roboto', sans-serif;
+    font-size: 26px;
+}
+
+.descripTextBody {
+    /* background: orange; */
+    text-align: center;
+}
+</style>
